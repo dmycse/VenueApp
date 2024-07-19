34d1d2a92be1afb7c10d7ff7402674cac591df00
dmycse <dmycse@gmail.com>
2024-07-19 18:11:05 +0300

    Set up clerk lib

---
+++ middleware.ts
@@ -0,0 +1,30 @@
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+
+// export default clerkMiddleware();
+
+const isProtectedRoute = createRouteMatcher([
+  '/dashboard(.*)',
+  '/forum(.*)',
+]);
+
+export default clerkMiddleware((auth, req) => {
+  if (!auth().userId && isProtectedRoute(req)) {
+
+    // Add custom logic to run before redirecting
+
+    return auth().redirectToSignIn();
+  }
+});
+
+// const isPublicRoute = createRouteMatcher([
+//   '/', '/events/:id', '/api/hooks/clerk', '/api/hook/stripe', 'api/uploadthing']);
+
+// export default clerkMiddleware((auth, request) => {
+//   if(!isPublicRoute(request)) {
+//     auth().protect();
+//   }
+// });
+
+export const config = {
+  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+};
