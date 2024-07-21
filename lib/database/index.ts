c5678e210f752117401d951adb7a971023b33b85
dmycse <dmycse@gmail.com>
2024-07-21 17:14:47 +0300

    Create connection to MongoDB

---
+++ lib/database/index.ts
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose';
+
+const MONGODB_URI = process.env.MONGODB_URI;
+
+let cached = (global as any).mongoose || {connection: null, promise: null};
+
+export const connectToDatabase = async () => {
+  if (cached.connection) return cached.connection;
+
+  if (!MONGODB_URI) throw new Error('MongoDB URI is missing');
+
+  cached.promise = cached.promise || mongoose.connect(MONGODB_URI, {
+    dbName: 'VenuelyDB',
+    bufferCommands: false
+  })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.log(`DB connection error: ${err}`));
+
+  cached.connection = await cached.promise;
+  console.log('cached.connection', cached.connection)
+  return cached.connection;
+}
