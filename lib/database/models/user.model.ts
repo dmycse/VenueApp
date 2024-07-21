ef471fc3c61733a07bfb28a35f910e017b06c685
dmycse <dmycse@gmail.com>
2024-07-21 22:06:40 +0300

    Refactor user.models

--- lib/database/models/user.model.ts
+++ lib/database/models/user.model.ts
@@ -4,8 +4,8 @@ const UserSchema = new Schema({
   clerkId: { type: String, required: true, unique: true },
   email: { type: String, required: true, minLength: 5, lowercase: true, unique: true },
   username: { type: String, required: true, unique: true },
-  firstName: { type: String, required: true, default: 'firstName' },
-  lastName: { type: String, required: true, default: 'lasttName' },
+  firstName: { type: String },
+  lastName: { type: String },
   photo: { type: String, required: true }
 });
 
